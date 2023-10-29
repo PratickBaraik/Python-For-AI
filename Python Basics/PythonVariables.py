7a3795bb2970ab534ab29b273c15dd3bbce1c64e
Pratick Baraik <70568435+PratickB111@users.noreply.github.com>
2023-10-29 12:28:04 +0530

    Adding basic python code
    This code shows ways of declaring variables in python

---
+++ Python Basics/PythonVariables.py
@@ -0,0 +1,54 @@
+# python variable declaration code
+myFirstVariable = "Hello python"
+print(myFirstVariable)
+
+# valid variable name in python
+my_First_Name = "Pratick"
+_my_Id = 165
+NumberGreakerThanZero = 1
+my_Location_ = "India"
+_Team_ = "Python for AI"
+print(my_First_Name, _my_Id)
+print(NumberGreakerThanZero, _Team_, my_Location_)
+
+# declaration and initialization in python 
+# variable assignments in python
+Num = 1231 # integer assignment
+PI = 3.14 # floating point number assignment
+Roll = "Student" # string assignment
+print(Num)
+print(PI)
+print(Roll)
+
+# redeclaration of variables in python
+Name = 'Github' # declaring the variable
+print(Name) # display
+Name = 'Python For AI' # re-declaring the variable
+print(Name) # display again
+
+# assigning multiple variables
+first = sec = third = 1000
+print(first)
+print(sec)
+print(third)
+
+mac = linux = windows = "Operating System"
+print(mac)
+print(linux)
+print(windows)
+
+# assigning different values to multiple variables
+# re-assigning 
+first, sec, third = 1, 2, 3
+print(first)
+print(sec)
+print(third)
+
+# use of + operator with variables
+first = 10
+sec = 30
+print(first + sec)
+
+first = "Pratick"
+sec = "Baraik"
+print(first + sec)
